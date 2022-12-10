d0d4d25696b55b39719b40d1fd145f5983e97e09
Bharat Reddy <breddy49@gmail.com>
2022-12-10 10:56:39 -0800

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //index.js
+
+module.exports = function getAppname(string) {
+    const myRe = /\.(.*?)\./;
+    const myRe2 = /\/\/(.*?)\./;
+    const checkRegEx = myRe.exec(string)[1].toString();
+    const checkRegEx2 = myRe2.exec(string)[1];
+    console.log("check for https: " + checkRegEx2);
+    return checkRegEx;
+  };
+
+
+  console.log(
+    "This is the App: " +
+      getAppname("https://www.t-tricks.com/snippets/css/a-guide-to-flexbox/")
+  );
