862e31223a4d0935e13e81552d0679fd6649428b
Bharat Reddy <breddy49@gmail.com>
2022-12-10 11:21:08 -0800

    updated tests and index

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 //index.js
 
-module.exports = function getAppname(string) {
+function getAppname(string) {
+
+    let Appname; 
+    //Regex for www. --- .com URLS
     const myRe = /\.(.*?)\./;
+    //Regex for http:// --- .com URLS
     const myRe2 = /\/\/(.*?)\./;
-    const checkRegEx = myRe.exec(string)[1].toString();
-    const checkRegEx2 = myRe2.exec(string)[1];
-    console.log("check for https: " + checkRegEx2);
-    return checkRegEx;
+
+    //Run both Regex options on the URL String
+    const checkRegEx = myRe.exec(string)//[1].toString();
+    const checkRegEx2 = myRe2.exec(string)//[1].toString();
+
+    //if it is a "www" URL, return the App string
+    if (checkRegEx) {
+         Appname = myRe.exec(string)[1].toString();
+    }
+    //if it is an "http" URL, return the App string
+    else if (checkRegEx2) {
+         Appname = myRe2.exec(string)[1].toString();
+
+    }
+    //if the String is in any other format, return the word "link"
+    else {
+         Appname = "link"
+    }
+
+    return Appname;
   };
 
 
-  console.log(
-    "This is the App: " +
-      getAppname("https://www.t-tricks.com/snippets/css/a-guide-to-flexbox/")
-  );
+
+  module.exports = getAppname
---
+++ tests.js
@@ -0,0 +1,13 @@
+const getAppname = require('getAppname')
+
+console.log(getAppname("www.digg.com"))
+
+console.log(getAppname("https://t-tricks.com/snippets/css/a-guide-to-flexbox/"))
+
+console.log(getAppname("google.com"))
+
+console.log(getAppname("https://codesandbox.io/"))
+
+console.log(getAppname("https://www.freecodecamp.org/news/how-to-make-a-beautiful-tiny-npm-package-and-publish-it-2881d4307f78/"))
+
+console.log(getAppname("https://docs.google.com/document/d/1HinbGPcuQgFzBCUdRCLgd3VV8FUD81ehT5y2xRPYWic/edit"))
